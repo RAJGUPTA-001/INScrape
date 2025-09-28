c281a7c1f61c83114f3709058ee9502212135b32
RAJGUPTA-001 <raj.gupta101r@gmail.com>
2025-09-29 02:49:58 +0530

    building...

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,35 @@
 // const express = require("express")
 import express from "express";
 import dotenv from "dotenv";
+import path, { dirname } from "path";
+
 dotenv.config();
 const port = process.env.PORT||3000;
 
 const app = express()
- 
+const __dirname = path.resolve(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    
+    app.get("*",(_,res)=>{
+        res.send(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+
+}
 app.listen(port,()=>console.log('server at '+port))
-app.get('/',(req,res)=>{
-    res.send("hii")
-})
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function homepage() {
+  return (
+    <div>homepage
+      <script>console.log("working")</script>
+    </div>
+  )
+}
+
+export default homepage
